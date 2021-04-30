e3b646cd5db8c8c3f94fbf0dd9464306b22ef667
Heidi Engstrom <heidi@embark.com>
2021-04-29 20:02:25 -0400

    update config

--- api/db/config/config.js
+++ api/db/config/config.js
@@ -4,21 +4,30 @@ module.exports = {
     password: "postgres",
     database: "notions_development",
     host: "localhost",
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: true
+    }
   },
 
 }
