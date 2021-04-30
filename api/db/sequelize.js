feae2dab42a1031a1f43dd70709f16a28f2921c3
Heidi Engstrom <heidi@embark.com>
2021-04-29 20:00:51 -0400

    another change to sequelize config

--- api/db/sequelize.js
+++ api/db/sequelize.js
@@ -4,12 +4,10 @@ console.log('url', process.env.DATABASE_URL)
 
 const sequelize = new Sequelize(
     process.env.DATABASE_URL, {
-    'dialect': 'postgres',
-    'dialectOptions': {
-        'ssl': true
-    },
-    'protocol': 'postgres'
-}
+        dialectOptions: {
+            ssl: true
+        }
+    }
 )
 
 
